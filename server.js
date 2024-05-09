89d953db77a3a4da4e5fc1b628c25bd54fb9572f
Vipul Kumar <102830320+smilevipul@users.noreply.github.com>
2024-05-10 04:36:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
+import {fileURLToPath} from 'url';
 
 //configure env
 dotenv.config();
