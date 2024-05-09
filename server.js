78a81c0f8f71dfb64968ed7fc3dddda3cc19583d
Vipul Kumar <102830320+smilevipul@users.noreply.github.com>
2024-05-10 04:39:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ dotenv.config();
 //databse config
 connectDB();
 
+//es module fix
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 //rest object
 const app = express();
 
